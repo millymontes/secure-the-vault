6fc71aefeb1a002d87b1ba5e51476d2fa2983e16
millymontes <millybriannaa@gmail.com>
2025-07-24 22:22:02 -0600

    adding js

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = (90 - 10) / 2;
+
+let code3 = 40 - (10 % 3);
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + code2 + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
